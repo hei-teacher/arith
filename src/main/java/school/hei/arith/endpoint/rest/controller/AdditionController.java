9110669fe40addba181017b516e7ccd7b9dfc6ff
Lou Andria <lou@hei.school>
2025-07-15 09:54:56 +0300

    chore: warn that negative inputs are not allowed

--- src/main/java/school/hei/arith/endpoint/rest/controller/AdditionController.java
+++ src/main/java/school/hei/arith/endpoint/rest/controller/AdditionController.java
@@ -1,13 +1,18 @@
 package school.hei.arith.endpoint.rest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class AdditionController {
   @GetMapping("/add")
   public long addition(@RequestParam long a, @RequestParam long b) {
+    if (a < 0 || b < 0) {
+      log.warn("Negative inputs are not allowed");
+    }
     return a + b;
   }
 }
